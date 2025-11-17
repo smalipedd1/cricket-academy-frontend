ef7f6cbee455d4829f9dcd6edf43c3604ed30b68
smalipedd1 <shasi.malipeddi@gmail.com>
2025-11-17 09:59:20 -0500

    Coach access to player eval response 4

--- src/App.js
+++ src/App.js
@@ -60,12 +60,8 @@ function AppRoutes() {
 	<Route path="/enter-dob" element={<EnterDOB />} />
 	<Route path="/player-dashboard" element={<PlayerDashboard />} />
 	<Route path="/coach/evaluation" element={<CoachEvaluationForm />} />
-	<Route path="/coach/evaluations/player" element={token && role === 'coach' ? <CoachPlayerEvaluations viewer="coach" />}/>
-
-<Route
-  path="/player/session/:id"
-  element={token && role === 'player' ? <CoachPlayerEvaluations viewer="player" /> : <Navigate to="/login" />}
-/>
+	<Route path="/coach/evaluations/player" element={token && role === 'coach' ? <CoachPlayerEvaluations />}/>
+	<Route path="/coach/evaluations/player" element={token && role === 'player' ? <CoachPlayerEvaluations /> : <Navigate to="/login" />}/>
     </Routes>
   );
 }
