c8b28b5a9a7173fd91dacde40f9a7ccc0ca25502
smalipedd1 <shasi.malipeddi@gmail.com>
2025-11-22 05:25:34 -0500

    Update to Admin dashboard. Use Patch instead of Put for updating players and coaches

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -79,7 +79,7 @@ const AdminDashboard = () => {
 
   const handleCoachUpdate = () => {
     const token = localStorage.getItem('token');
-    const method = isAddingCoach ? 'post' : 'put';
+    const method = isAddingCoach ? 'post' : 'patch';
     const url = isAddingCoach
       ? 'https://cricket-academy-backend.onrender.com/api/admin/coaches'
       : `https://cricket-academy-backend.onrender.com/api/admin/coaches/${editedCoach._id}`;
@@ -108,7 +108,7 @@ const AdminDashboard = () => {
 
   const handlePlayerUpdate = () => {
     const token = localStorage.getItem('token');
-    const method = isAddingPlayer ? 'post' : 'put';
+    const method = isAddingPlayer ? 'post' : 'patch';
     const url = isAddingPlayer
       ? 'https://cricket-academy-backend.onrender.com/api/admin/players'
       : `https://cricket-academy-backend.onrender.com/api/admin/players/${editedPlayer._id}`;
